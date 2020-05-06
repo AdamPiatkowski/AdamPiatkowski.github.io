f061947a289ae798b6f32fe863ca7ff99e2263e3
AdamPiatkowski <picus1991@gmail.com>
2020-05-06 13:09:31 +0100

    poprawka

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://aqueous-refuge-65564.herokuapp.com/v1/task/';
+  var apiRoot = 'https://aqueous-refuge-65564.';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
